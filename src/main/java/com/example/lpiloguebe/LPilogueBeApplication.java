c9e03a69dbfa6377d0e49d7cadf6e8739e0e85d5
wlswnsdn <wlswnsdn123@gmail.com>
2024-12-03 15:35:34 +0900

    initial commit

---
+++ src/main/java/com/example/lpiloguebe/LPilogueBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.lpiloguebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LPilogueBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LPilogueBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lpiloguebe/LPilogueBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lpiloguebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LPilogueBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
